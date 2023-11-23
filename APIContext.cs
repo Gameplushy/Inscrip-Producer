f8bc67707dca1603cf9cde55342e93883f76ed8b
Gameplushy <victor_f@orange.fr>
2023-11-23 16:49:10 +0100

    Basic REST API construction

---
+++ APIContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Channels;
+
+namespace Inscrip
+{
+    public class APIContext : DbContext
+    {
+        public DbSet<Inscription> Inscriptions { get; set; }
+
+        public APIContext(DbContextOptions<APIContext> options) : base(options) { }
+
+    }
+}
---
+++ Inscription.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Inscrip
+{
+    public class Inscription
+    {
+        [Key]
+        public required string MailAddress { get; set; }
+        public required string FullName { get; set; }
+    }
+}
